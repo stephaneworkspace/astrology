8d6e5d4f244873286cf3515b0f63f8491ffcc138
stephaneworkspace <s.bressani@bluewin.ch>
2019-12-23 00:46:40 +0100

    This time is working partialy

--- lib/libswe-sys/src/lib.rs
+++ lib/libswe-sys/src/lib.rs
@@ -1,22 +1,28 @@
 use std::env;
-use std::os::raw::c_char;
-//use std::ptr;
+//use std::ffi::CString;
+use std::os::raw::c_uchar;
+use std::ptr;
 mod raw;
 pub fn test_lib() -> String {
     // Initial path swissephem
     let path_begin = env::var("CARGO_MANIFEST_DIR").unwrap();
     let path_full = path_begin + "/lib/libswe-sys/src/swisseph/sweph";
-    //let path_final = b"./swisseph/sweph".as_ptr() as *const c_char;
-    let path_final = path_full.as_bytes().as_ptr() as *const c_char;
-    let version: *mut c_char = "\0".as_bytes().as_ptr() as *mut c_char;
-    //let version = ptr::null_mut() as *mut c_char;
+    //let path_final = b"./swisseph/sweph".as_ptr() as *const c_uchar;
+    let path_final = path_full.as_bytes().as_ptr() as *const c_uchar;
+    let version = ptr::null_mut() as *mut c_uchar;
+    //let version: *mut c_uchar = "\0".as_bytes().as_ptr() as *mut c_uchar;
+    //let version_for_ferry: Vec<u8>;
+    let version = ptr::null_mut() as *mut c_uchar;
     unsafe {
         raw::swe_set_ephe_path(path_final);
         // Get the version
-        raw::swe_version(version);
+        raw::swe_version(&version);
+        // let test_unsafe = CString::from_raw(version as *mut i8).as_bytes();
+        //version_for_ferry = CString::from_raw(version as *mut i8);
         // Free memory
         raw::swe_close();
     }
     // Return to rusty
+    // path_full
     path_full
 }
--- lib/libswe-sys/src/raw.rs
+++ lib/libswe-sys/src/raw.rs
@@ -3,14 +3,14 @@
 #![allow(non_snake_case)
 */
 
-use std::os::raw::c_char;
+use std::os::raw::c_uchar;
 
 #[link(name = "swe")]
 extern "C" {
     // pub fn swe_test(path: *const c_char); // swe_test try
-    pub fn swe_set_ephe_path(path: *const c_char);
+    pub fn swe_set_ephe_path(path: *const c_uchar);
     /// Version
-    pub fn swe_version(s_version: *mut c_char) -> *mut c_char;
+    pub fn swe_version(s_version: &*mut c_uchar) -> *mut c_uchar;
     /// Free memory
     pub fn swe_close();
 }
