4f3a7905fb06e5cdeb4eba266e38f9b0969dbcb1
stephaneworkspace <s.bressani@bluewin.ch>
2019-12-25 00:45:19 +0100

    Prepare binary with version

--- lib/libswe-sys/src/lib.rs
+++ lib/libswe-sys/src/lib.rs
@@ -2,6 +2,22 @@ use std::env;
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 mod raw;
+/// Get version
+pub fn get_version() -> CString {
+    // Get the version
+    let mut version = [0; 255];
+    let v = unsafe {
+        let p = version.as_mut_ptr();
+        raw::swe_version(p);
+        CStr::from_ptr(p)
+    };
+    // Memory clean
+    unsafe {
+        raw::swe_close();
+    }
+    CString::from(v)
+}
+
 pub fn test_lib() -> String {
     // Initial path swissephem
     let path_begin = env::var("CARGO_MANIFEST_DIR").unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,26 @@
 extern crate ferris_says;
 
+use libswe_sys::get_version;
+use std::ffi::CString;
 use std::io::{stdout, BufWriter};
 
 /// Simple write in console
 #[no_mangle]
 pub extern "C" fn example_intro() {
-    let phrase = b"Welcome to astro_compute_swisseph";
+    let phrase = b"Welcome to astro_compute_swisseph version";
     let stdout = stdout();
     let mut writer = BufWriter::new(stdout.lock());
     ferris_says::say(phrase, 40, &mut writer).unwrap();
 }
 
+/// Return version of api
+#[no_mangle]
+pub extern "C" fn version() -> CString {
+    get_version()
+}
+
+/// This code is not detect by lipo
+/// I think &[u8] is not valid
 #[no_mangle]
 pub extern "C" fn example_from_lib(phrase: &[u8]) {
     let stdout = stdout();
