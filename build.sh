a60f4eb8a4c7103b105a50846fd5f9c6c13dbc93
stephaneworkspace <s.bressani@bluewin.ch>
2019-12-25 00:31:30 +0100

    Prepare binary

--- build.sh
+++ build.sh
@@ -5,4 +5,15 @@ cd ../..
 cargo build
 #cargo test
 cargo run --example ferris-astro-example
-# --verbose
+
+# Group binary with lipo
+cargo build --target aarch64-apple-ios
+cargo build --target x86_64-apple-ios
+# Print NonFat -> Ok
+lipo -info ./target/aarch64-apple-ios/debug/libastro_compute_swisseph.a
+lipo -info ./target/x86_64-apple-ios/debug/libastro_compute_swisseph.a
+
+# Group in one
+lipo -create ./target/aarch64-apple-ios/debug/libastro_compute_swisseph.a ./target/x86_64-apple-ios/debug/libastro_compute_swisseph.a -output ./target/astro_compute_swisseph.a
+# Print architecture
+lipo -info ./target/astro_compute_swisseph.a
