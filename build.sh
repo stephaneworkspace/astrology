e7820284e0048aed46c012bc332a81398eebe19a
stephaneworkspace <s.bressani@bluewin.ch>
2019-12-25 11:52:40 +0100

    This time without call witout lib c swisseph for debug

--- build.sh
+++ build.sh
@@ -10,10 +10,10 @@ cargo run --example ferris-astro-example
 cargo build --target aarch64-apple-ios
 cargo build --target x86_64-apple-ios
 # Print NonFat -> Ok
-lipo -info ./target/aarch64-apple-ios/debug/libastro_compute_swisseph.a
-lipo -info ./target/x86_64-apple-ios/debug/libastro_compute_swisseph.a
+lipo -info ./target/aarch64-apple-ios/debug/libastro.a
+lipo -info ./target/x86_64-apple-ios/debug/libastro.a
 
 # Group in one
-lipo -create ./target/aarch64-apple-ios/debug/libastro_compute_swisseph.a ./target/x86_64-apple-ios/debug/libastro_compute_swisseph.a -output ./target/libastro_compute_swisseph.a
+lipo -create ./target/aarch64-apple-ios/debug/libastro.a ./target/x86_64-apple-ios/debug/libastro.a -output ./target/libastro.a
 # Print architecture
-lipo -info ./target/libastro_compute_swisseph.a
+lipo -info ./target/libastro.a
