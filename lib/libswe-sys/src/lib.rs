6bfd8e73c45fd4e84a6a90438d48d80dddffa178
stephaneworkspace <s.bressani@bluewin.ch>
2019-12-26 16:08:14 +0100

    Clean code

--- lib/libswe-sys/src/lib.rs
+++ lib/libswe-sys/src/lib.rs
@@ -15,50 +15,5 @@
  * projects, you must adhere to the GPL license or buy a Swiss Ephemeris
  * commercial license.
  */
-use std::env;
-use std::ffi::{CStr, CString};
-use std::os::raw::c_char;
 mod raw;
 pub mod swerust;
-
-/// Get version
-pub fn get_version() -> CString {
-    // Get the version
-    let mut version = [0; 255];
-    let v = unsafe {
-        let p = version.as_mut_ptr();
-        raw::swe_version(p);
-        CStr::from_ptr(p)
-    };
-    // Memory clean
-    unsafe {
-        raw::swe_close();
-    }
-    CString::from(v)
-}
-
-pub fn test_lib() -> String {
-    // Initial path swissephem
-    let path_begin = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let path_full = path_begin + "/lib/libswe-sys/src/swisseph/sweph";
-    //let path_final = b"./swisseph/sweph".as_ptr() as *const c_char;
-    let path_final = path_full.as_bytes().as_ptr() as *const c_char;
-    unsafe {
-        raw::swe_set_ephe_path(path_final);
-    }
-
-    // Get the version
-    let mut version = [0; 255];
-    let v = unsafe {
-        let p = version.as_mut_ptr();
-        raw::swe_version(p);
-        CStr::from_ptr(p)
-    };
-
-    // Memory clean
-    unsafe {
-        raw::swe_close();
-    }
-
-    CString::from(v).to_str().unwrap().to_string()
-}
