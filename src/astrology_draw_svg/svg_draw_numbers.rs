58454c1370751b7b8f4ad26b5f9b596b443e6c77
stephaneworkspace <s.bressani@bluewin.ch>
2020-02-18 14:54:36 +0100

    add 3' svg

--- src/astrology_draw_svg/svg_draw_numbers.rs
+++ src/astrology_draw_svg/svg_draw_numbers.rs
@@ -2043,6 +2043,51 @@ pub fn draw_minute(minute: i16) -> Document {
                 .set("viewBox", (0, 0, size.0, size.1))
                 .add(group);
         },
+        3 => {
+            size = (50.0, 50.0);
+            let data1 = Data::new()
+                .move_to((15.0, 35.3)) // M
+                .cubic_curve_by((1.0, 0.6, 3.2, 1.6, 5.6, 1.6)) // c
+                .cubic_curve_by((4.4, 0.0, 5.7, -2.8, 5.7, -4.9)) // c
+                .cubic_curve_by((0.0, -3.5, -3.2, -5.0, -6.5, -5.0)) // c
+                .horizontal_line_by(-1.9) // h
+                .vertical_line_by(-2.5) // v
+                .horizontal_line_by(1.9) // h
+                .cubic_curve_by((2.5, 0.0, 5.6, -1.3, 5.6, -4.2)) // c
+                .cubic_curve_by((0.0, -2.0, -1.3, -3.8, -4.4, -3.8)) // c
+                .cubic_curve_by((-2.0, 0.0, -3.9, 0.9, -5.0, 1.7)) // c
+                .line_to((15.0, 15.6)) // L
+                .cubic_curve_by((1.3, -1.0, 3.9, -1.9, 6.6, -1.9)) // c
+                .cubic_curve_by((4.9, 0.0, 7.2, 2.9, 7.2, 6.0)) // c
+                .cubic_curve_by((0.0, 2.6, -1.5, 4.8, -4.6, 5.9)) // c
+                .vertical_line_by(0.1) // v
+                .cubic_curve_by((3.1, 0.6, 5.6, 2.9, 5.6, 6.5)) // c
+                .cubic_curve_by((0.0, 4.0, -3.1, 7.5, -9.2, 7.5)) // c
+                .cubic_curve_by((-2.8, 0.0, -5.3, -0.9, -6.5, -1.7)) // c
+                .line_to((15.0, 35.3)) // L
+                .close(); // z
+            let data2 = Data::new()
+                .move_to((39.4, 12.7)) // M
+                .cubic_curve_by((-0.9, 3.2, -2.5, 7.4, -3.6, 9.2)) // c
+                .line_by((-2.2, 0.3)) // l
+                .cubic_curve_by((0.8, -2.3, 1.9, -6.3, 2.3, -9.1)) // c
+                .line_to((39.4, 12.7)) // L
+                .close(); // z
+            let path1 = Path::new()
+                .set("fill", "black")
+                .set("stroke", "black")
+                .set("stroke-width", 0)
+                .set("d", data1);
+            let path2 = Path::new()
+                .set("fill", "black")
+                .set("stroke", "black")
+                .set("stroke-width", 0)
+                .set("d", data2);
+            let group = Group::new().add(path1).add(path2);
+            document = Document::new()
+                .set("viewBox", (0, 0, size.0, size.1))
+                .add(group);
+        },
         _ => {
             size = (50.0, 50.0);
             document = Document::new().set("viewBox", (0, 0, size.0, size.1));
