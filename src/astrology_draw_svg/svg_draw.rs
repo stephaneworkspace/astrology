8c66a81b9bf7d77cbc007068d9ee5474254517fb
stephaneworkspace <s.bressani@bluewin.ch>
2020-03-17 17:16:46 +0100

    zodiac offset bug fix

--- src/astrology_draw_svg/svg_draw.rs
+++ src/astrology_draw_svg/svg_draw.rs
@@ -224,10 +224,18 @@ impl Draw for WorkingStorageDraw {
             let sign = i as i32;
             // 0°
             // temporary Aries 0°0'0"
-            let off_pos_asc: f32 = self.ws.house[0].longitude as f32;
-            let mut pos = (sign as f32 - 0.0) * 30.0 + &off_pos_asc;
-            if pos > 360.0 {
-                pos = pos - 360.0;
+            let off_pos_asc: f32 = 360.0 - self.ws.house[0].longitude as f32;
+            let mut pos = sign as f32 * 30.0 + &off_pos_asc;
+            let mut done = false;
+            while !done {
+                if pos > 360.0 {
+                    pos = pos - 360.0;
+                }
+                if pos > 360.0 {
+                    //
+                } else {
+                    done = true
+                }
             }
             // let pos = sign_pos_circle;
             let a_xy: [Offset; 2] = self.ws.get_line_trigo(
@@ -251,14 +259,17 @@ impl Draw for WorkingStorageDraw {
                 } else {
                     larger_draw_line = LargerDrawLine::Small;
                 }
-                // temporary Aries 0°0'0"
-                pos = ((sign as f32 - 0.0) * 30.0)
-                    + (j as f32 * 2.0)
-                    + &off_pos_asc;
-                // to do
-                // pos = sign_pos_circle + (j as f32 * 2.0);
-                if pos > 360.0 {
-                    pos = pos - 360.0;
+                pos = (sign as f32 * 30.0) + (j as f32 * 2.0) + &off_pos_asc;
+                done = false;
+                while !done {
+                    if pos > 360.0 {
+                        pos = pos - 360.0;
+                    }
+                    if pos > 360.0 {
+                        //
+                    } else {
+                        done = true
+                    }
                 }
                 let a_xy_line: [Offset; 2] = self.ws.get_line_trigo(
                     pos,
@@ -376,7 +387,7 @@ impl Draw for WorkingStorageDraw {
         let zodiac_size =
             (((ZODIAC_SIZE * zodiac_ratio) / 100.0) * self.ws.max_size) / 100.0;
 
-        let off_pos_asc: f32 = self.ws.house[0].longitude as f32;
+        let off_pos_asc: f32 = 360.0 - self.ws.house[0].longitude as f32;
         let mut pos =
             ((sign.clone() as u64 + 1) as f32 * 30.0) + 15.0 + &off_pos_asc;
         let mut done = false;
