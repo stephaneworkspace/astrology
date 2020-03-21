4c3d44e454694ffd13c7b6e4526875564dd1327a
stephaneworkspace <s.bressani@bluewin.ch>
2020-03-21 17:09:31 +0100

    add compile for node.js

---
+++ build_nodejs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+cargo build
+cargo test
+
+# Group binary with lipo
+cargo build --target x86_64-apple-darwin
+cargo build --target x86_64-unknown-linux-gnu
+cargo build --target x86_64-pc-windows-gnu
+# Print NonFat -> Ok
+lipo -info ./target/x86_64-apple-dawrin/debug/libastrology.a
+lipo -info ./target/x86_64-unknown-linux-gnu/debug/libastrology.a
+lipo -info ./target/x86_64-pc-windows-gnu/debug/libastrology.a
+
+# Group in one
+lipo -create ./target/x86_64_apple-darwin/debug/libastrology.a ./target/x86_64-unknown-linux-gnu/debug/libastrology.a ./target/x86_64-pc-windows-gnu/debug/libastrology.a -output ./target/libastrology.a
+# Print architecture
+lipo -info ./target/libastrology.a
