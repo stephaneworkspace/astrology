230719902dff294f2dc11a92672a014e81da2bfb
stephaneworkspace <s.bressani@bluewin.ch>
2020-03-25 13:49:42 +0100

    add aspects algo begining

--- src/astrology_draw_svg/mod.rs
+++ src/astrology_draw_svg/mod.rs
@@ -355,6 +355,66 @@ pub fn chart(
                     });
                     asp_vec.clear();
                 }
+                // Trine 120° - orbe 7°
+                asp = 120;
+                if (abs_separation - asp as f32).abs() <= 7.0 {
+                    asp_vec.push(Aspects::Trine);
+                    let draw = ws_draw.draw_aspect(
+                        bodie.longitude as f32,
+                        b.longitude as f32,
+                        Aspects::Trine,
+                    );
+                    res.push(DataObjectSvg {
+                        svg: draw.svg,
+                        object_type: DataObjectType::Aspect,
+                        size_x: draw.size_x as f32,
+                        size_y: draw.size_y as f32,
+                        pos_x: draw.pos_x as f32,
+                        pos_y: draw.pos_y as f32,
+                        aspects: asp_vec.clone(),
+                    });
+                    asp_vec.clear();
+                }
+                // Square 90° - orbe 6°
+                asp = 90;
+                if (abs_separation - asp as f32).abs() <= 6.0 {
+                    asp_vec.push(Aspects::Square);
+                    let draw = ws_draw.draw_aspect(
+                        bodie.longitude as f32,
+                        b.longitude as f32,
+                        Aspects::Square,
+                    );
+                    res.push(DataObjectSvg {
+                        svg: draw.svg,
+                        object_type: DataObjectType::Aspect,
+                        size_x: draw.size_x as f32,
+                        size_y: draw.size_y as f32,
+                        pos_x: draw.pos_x as f32,
+                        pos_y: draw.pos_y as f32,
+                        aspects: asp_vec.clone(),
+                    });
+                    asp_vec.clear();
+                }
+                // Sextile 60° - orbe 5°
+                asp = 60;
+                if (abs_separation - asp as f32).abs() <= 5.0 {
+                    asp_vec.push(Aspects::Sextile);
+                    let draw = ws_draw.draw_aspect(
+                        bodie.longitude as f32,
+                        b.longitude as f32,
+                        Aspects::Sextile,
+                    );
+                    res.push(DataObjectSvg {
+                        svg: draw.svg,
+                        object_type: DataObjectType::Aspect,
+                        size_x: draw.size_x as f32,
+                        size_y: draw.size_y as f32,
+                        pos_x: draw.pos_x as f32,
+                        pos_y: draw.pos_y as f32,
+                        aspects: asp_vec.clone(),
+                    });
+                    asp_vec.clear();
+                }
             }
         }
         for i in 0..12 {
