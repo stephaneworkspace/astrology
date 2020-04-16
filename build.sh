2b922cf34d4b4eabd4bfaec72e90c8a7042ba2cd
Stéphane Bressani <stephanebressani@macbook-air-de-stephane-1.home>
2020-04-17 00:56:04 +0200

    fix the header and remove unused

--- build.sh
+++ build.sh
@@ -16,3 +16,5 @@ lipo -info ./target/x86_64-apple-ios/debug/libastrology.a
 lipo -create ./target/aarch64-apple-ios/debug/libastrology.a ./target/x86_64-apple-ios/debug/libastrology.a -output ./target/libastrology.a
 # Print architecture
 lipo -info ./target/libastrology.a
+
+cbindgen . -o target/libastrology.h
