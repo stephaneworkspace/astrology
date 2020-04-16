933f627c2ada97623379cd335e4932aea6fd3561
stephaneworkspace <s.bressani@bluewin.ch>
2020-04-17 01:23:40 +0200

    fix the header

--- build.sh
+++ build.sh
@@ -17,4 +17,19 @@ lipo -create ./target/aarch64-apple-ios/debug/libastrology.a ./target/x86_64-app
 # Print architecture
 lipo -info ./target/libastrology.a
 
-cbindgen . -o target/libastrology.h
+#cbindgen . -o target/libastrology.h
+
+# -> conflict with docs.rs
+
+#extern crate cbindgen;
+
+#use std::env;
+
+#fn main() {
+#    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+#    let mut config: cbindgen::Config = Default::default();
+#    config.language = cbindgen::Language::C;
+#    cbindgen::generate_with_config(&crate_dir, config)
+#        .unwrap()
+#        .write_to_file("target/libastrology.h");
+#}
