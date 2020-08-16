ae1a94d6d1daff8afb819fa6b19b8cbcfcc54d15
stephaneworkspace <s.bressani@bluewin.ch>
2020-08-16 16:45:33 +0200

    more clap validator

--- src/cfg/validator.rs
+++ src/cfg/validator.rs
@@ -1,3 +1,13 @@
+use std::path::Path;
+
+/// Check if the path for the swissephem files exist
 pub fn parse_path(path: String) -> Result<(), String> {
-    Ok(())
+    if Path::new(path.clone().as_str()).exists() {
+        Ok(())
+    } else {
+        Err(format!(
+            "The path for your swiss emphem files don't exist: {}",
+            path
+        ))
+    }
 }
