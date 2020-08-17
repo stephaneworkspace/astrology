c84d7fecc6d067c7d2de4683b7b58115424fe247
stephaneworkspace <s.bressani@bluewin.ch>
2020-08-17 17:14:36 +0200

    clean code

--- src/svg_draw/compute_chart.rs
+++ src/svg_draw/compute_chart.rs
@@ -15,6 +15,10 @@
  * adhere to the GPL license or buy a Swiss Ephemeris commercial license.
  */
 extern crate libswe_sys;
+use super::aspects::{
+    aspects_all_aspects, aspects_draw, aspects_maj_aspects,
+    aspects_min_aspects, aspects_no_aspect,
+};
 use crate::svg_draw::{
     CalcDraw, Draw, WorkingStorageDrawPolyMorphNatal,
     WorkingStorageDrawPolyMorphTransit, WorkingStoragePolyMorphNatal,
@@ -915,4 +919,103 @@ pub fn chart_svg(
     }
     svg_res = format!("{}</svg>", svg_res);
     svg_res
+}
+
+pub fn all_aspects(lang: Language) -> Vec<DataObjectAspectSvg> {
+    let mut res: Vec<DataObjectAspectSvg> = Vec::new();
+    // No aspect
+    let va_no_aspect: Vec<Aspects> = Vec::new();
+    let mut t;
+    t = match lang {
+        // To do const
+        Language::English => "No aspect".to_string(),
+        Language::French => "Pas d'aspect".to_string(),
+    };
+    res.push(DataObjectAspectSvg {
+        svg: aspects_no_aspect(Theme::Light, lang).to_string(),
+        text: t,
+        aspects: va_no_aspect,
+    });
+
+    // Maj aspects
+    let mut va_maj_aspects: Vec<Aspects> = Vec::new();
+    for a in Aspects::iter() {
+        if a.maj() {
+            va_maj_aspects.push(a.clone());
+        }
+    }
+    t = match lang {
+        // To do const
+        Language::English => "Majors aspects".to_string(),
+        Language::French => "Aspects majeurs".to_string(),
+    };
+    res.push(DataObjectAspectSvg {
+        svg: aspects_maj_aspects(Theme::Light, lang).to_string(),
+        text: t,
+        aspects: va_maj_aspects,
+    });
+
+    // Single Maj aspects
+    for a in Aspects::iter() {
+        if a.maj() {
+            let mut va: Vec<Aspects> = Vec::new();
+            va.push(a as Aspects);
+            res.push(DataObjectAspectSvg {
+                svg: aspects_draw(a, Theme::Light, lang).to_string(),
+                text: a.text(lang),
+                aspects: va.clone(),
+            });
+            va.clear()
+        }
+    }
+
+    // Min aspects
+    let mut va_min_aspects: Vec<Aspects> = Vec::new();
+    for a in Aspects::iter() {
+        if !a.maj() {
+            va_min_aspects.push(a.clone());
+        }
+    }
+    t = match lang {
+        // To do const
+        Language::English => "Minors aspects".to_string(),
+        Language::French => "Aspects mineurs".to_string(),
+    };
+    res.push(DataObjectAspectSvg {
+        svg: aspects_min_aspects(Theme::Light, lang).to_string(),
+        text: t,
+        aspects: va_min_aspects,
+    });
+
+    // Single Min aspects
+    for a in Aspects::iter() {
+        if !a.maj() {
+            let mut va: Vec<Aspects> = Vec::new();
+            va.push(a as Aspects);
+            res.push(DataObjectAspectSvg {
+                svg: aspects_draw(a, Theme::Light, lang).to_string(),
+                text: a.text(lang),
+                aspects: va.clone(),
+            });
+            va.clear()
+        }
+    }
+
+    // All aspects
+    let mut va_all_aspects: Vec<Aspects> = Vec::new();
+    for a in Aspects::iter() {
+        va_all_aspects.push(a.clone());
+    }
+    t = match lang {
+        // To do const
+        Language::English => "All aspects".to_string(),
+        Language::French => "Tous les aspects".to_string(),
+    };
+    res.push(DataObjectAspectSvg {
+        svg: aspects_all_aspects(Theme::Light, lang).to_string(),
+        text: t,
+        aspects: va_all_aspects,
+    });
+
+    res
 }
--- src/svg_draw/mod.rs
+++ src/svg_draw/mod.rs
@@ -18,19 +18,14 @@ extern crate base64;
 extern crate libswe_sys;
 extern crate serde;
 extern crate strum;
-use libswe_sys::sweconst::{Aspects, Language, Theme};
 pub mod angles;
 pub mod aspects;
 pub mod bodies;
+pub mod compute_chart;
 pub mod houses;
 pub mod numbers;
-pub mod zodiacs;
-use aspects::{
-    aspects_all_aspects, aspects_draw, aspects_maj_aspects,
-    aspects_min_aspects, aspects_no_aspect,
-};
-pub mod compute_chart;
 pub mod svg_draw;
+pub mod zodiacs;
 pub use self::compute_chart::{
     chart, chart_svg, chart_with_transit, DataChartNatal, DataObjectAspectSvg,
     DataObjectSvg, DataObjectType,
@@ -39,107 +34,7 @@ pub use self::svg_draw::{
     WorkingStorageDrawPolyMorphNatal, WorkingStorageDrawPolyMorphTransit,
     WorkingStoragePolyMorphNatal, WorkingStoragePolyMorphTransit,
 };
-use strum::IntoEnumIterator;
 pub use svg_draw::*;
-
-pub fn all_aspects(lang: Language) -> Vec<DataObjectAspectSvg> {
-    let mut res: Vec<DataObjectAspectSvg> = Vec::new();
-    // No aspect
-    let va_no_aspect: Vec<Aspects> = Vec::new();
-    let mut t;
-    t = match lang {
-        // To do const
-        Language::English => "No aspect".to_string(),
-        Language::French => "Pas d'aspect".to_string(),
-    };
-    res.push(DataObjectAspectSvg {
-        svg: aspects_no_aspect(Theme::Light, lang).to_string(),
-        text: t,
-        aspects: va_no_aspect,
-    });
-
-    // Maj aspects
-    let mut va_maj_aspects: Vec<Aspects> = Vec::new();
-    for a in Aspects::iter() {
-        if a.maj() {
-            va_maj_aspects.push(a.clone());
-        }
-    }
-    t = match lang {
-        // To do const
-        Language::English => "Majors aspects".to_string(),
-        Language::French => "Aspects majeurs".to_string(),
-    };
-    res.push(DataObjectAspectSvg {
-        svg: aspects_maj_aspects(Theme::Light, lang).to_string(),
-        text: t,
-        aspects: va_maj_aspects,
-    });
-
-    // Single Maj aspects
-    for a in Aspects::iter() {
-        if a.maj() {
-            let mut va: Vec<Aspects> = Vec::new();
-            va.push(a as Aspects);
-            res.push(DataObjectAspectSvg {
-                svg: aspects_draw(a, Theme::Light, lang).to_string(),
-                text: a.text(lang),
-                aspects: va.clone(),
-            });
-            va.clear()
-        }
-    }
-
-    // Min aspects
-    let mut va_min_aspects: Vec<Aspects> = Vec::new();
-    for a in Aspects::iter() {
-        if !a.maj() {
-            va_min_aspects.push(a.clone());
-        }
-    }
-    t = match lang {
-        // To do const
-        Language::English => "Minors aspects".to_string(),
-        Language::French => "Aspects mineurs".to_string(),
-    };
-    res.push(DataObjectAspectSvg {
-        svg: aspects_min_aspects(Theme::Light, lang).to_string(),
-        text: t,
-        aspects: va_min_aspects,
-    });
-
-    // Single Min aspects
-    for a in Aspects::iter() {
-        if !a.maj() {
-            let mut va: Vec<Aspects> = Vec::new();
-            va.push(a as Aspects);
-            res.push(DataObjectAspectSvg {
-                svg: aspects_draw(a, Theme::Light, lang).to_string(),
-                text: a.text(lang),
-                aspects: va.clone(),
-            });
-            va.clear()
-        }
-    }
-
-    // All aspects
-    let mut va_all_aspects: Vec<Aspects> = Vec::new();
-    for a in Aspects::iter() {
-        va_all_aspects.push(a.clone());
-    }
-    t = match lang {
-        // To do const
-        Language::English => "All aspects".to_string(),
-        Language::French => "Tous les aspects".to_string(),
-    };
-    res.push(DataObjectAspectSvg {
-        svg: aspects_all_aspects(Theme::Light, lang).to_string(),
-        text: t,
-        aspects: va_all_aspects,
-    });
-
-    res
-}
 
 #[cfg(test)]
 mod tests {
